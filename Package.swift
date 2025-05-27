6a16c1aea4988bcb309311d41d86cd6837dc2853
merteyup <eupmrt@gmail.com>
2025-05-27 10:37:02 +0300

    Add default platform requirements.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "EMOnboardingManager",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
