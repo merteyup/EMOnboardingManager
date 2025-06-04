bb7a3b21adf0a99419aca7d1909c8a6a00966161
merteyup <eupmrt@gmail.com>
2025-06-04 11:29:46 +0300

    Improve `MultipleChoiceView`'s design.

--- Sources/EMOnboardingManager/Components/MultipleChoiceView.swift
+++ Sources/EMOnboardingManager/Components/MultipleChoiceView.swift
@@ -14,26 +14,34 @@ public struct MultipleChoiceView: View {
     @State private var selectedIndex: Int? = nil
 
     public var body: some View {
-        VStack(spacing: Paddings.low) {
-            ForEach(options.indices, id: \.self) { index in
-                Button(action: {
-                    selectedIndex = index
-                    selected(index)
-                }) {
-                    HStack {
-                        Image(systemName: selectedIndex == index ? "largecircle.fill.circle" : "circle")
-                            .foregroundColor(.primary)
-                        Text(options[index])
-                            .foregroundColor(.primary)
-                        Spacer()
+        GeometryReader { geometry in
+            let deviceHeight = geometry.size.height
+            let spacing: CGFloat = deviceHeight > 750 ? Paddings.mid : 2
+            
+            VStack(spacing: spacing) {
+                ForEach(options.indices, id: \.self) { index in
+                    Button(action: {
+                        selectedIndex = index
+                        selected(index)
+                    }) {
+                        HStack {
+                            Image(systemName: selectedIndex == index ? "largecircle.fill.circle" : "circle")
+                                .foregroundColor(.primary)
+                            Text(options[index])
+                                .foregroundColor(.primary)
+                                .font(.callout)
+                            Spacer()
+                        }
+                        .padding(.vertical, 4)
                     }
-                    .padding(.vertical, 4)
+                    .buttonStyle(PlainButtonStyle())
                 }
-                .buttonStyle(PlainButtonStyle())
             }
-        }.frame(maxWidth: .infinity, alignment: .leading)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
     }
 }
+
 
 #Preview {
     MultipleChoiceView(
